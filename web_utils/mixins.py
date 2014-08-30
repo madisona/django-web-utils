93e5e38c124c5742d481c9c9172d8f6e71c92dba
Aaron Madison <aaron.madison@imtapps.com>
2014-08-30 14:29:29 -0500

    Feature: Added Mixins for generic views

---
+++ web_utils/mixins.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.utils.cache import patch_response_headers
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page, never_cache
+from django.views.decorators.csrf import csrf_exempt
+
+# https://gist.github.com/cyberdelia/1231560
+
+class NeverCacheMixin(object):
+
+    @method_decorator(never_cache)
+    def dispatch(self, *args, **kwargs):
+        return super(NeverCacheMixin, self).dispatch(*args, **kwargs)
+ 
+ 
+class LoginRequiredMixin(object):
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+
+
+class StaffMemberRequiredMixin(object):
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super(StaffMemberRequiredMixin, self).dispatch(*args, **kwargs)
+
+ 
+class CSRFExemptMixin(object):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(CSRFExemptMixin, self).dispatch(*args, **kwargs)
+ 
+ 
+class CacheMixin(object):
+    cache_timeout = 60
+ 
+    def get_cache_timeout(self):
+        return self.cache_timeout
+ 
+    def dispatch(self, *args, **kwargs):
+        return cache_page(self.get_cache_timeout())(super(CacheMixin, self).dispatch)(*args, **kwargs)
+ 
+ 
+class CacheControlMixin(object):
+    cache_timeout = 60
+ 
+    def get_cache_timeout(self):
+        return self.cache_timeout
+ 
+    def dispatch(self, *args, **kwargs):
+        response = super(CacheControlMixin, self).dispatch(*args, **kwargs)
+        patch_response_headers(response, self.get_cache_timeout())
+        return response
