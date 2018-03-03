7b5564102d60ace1505601f0dc312ca0ff374f9e
Aaron Madison <aaron.l.madison@gmail.com>
2018-03-02 21:32:01 -0600

    Version: 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-web-utils",
-    version='0.2.2',
+    version='0.3.0',
     author="Aaron Madison",
     description="Django helpers for working with the web.",
     long_description=open('README', 'r').read(),
